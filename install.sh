d02ca2ac11474703d57cfbdd0e09ec1d8a732895
Benson Wong <mostlygeek@gmail.com>
2013-06-05 10:10:14 -0600

    Enable using mouse to resize pane

--- install.sh
+++ install.sh
@@ -128,7 +128,7 @@ bind-key -t vi-copy 'y' copy-selection
 
 # Enable mouse scrolling
 set -g mode-mouse on
-#set -g mouse-resize-pane on
+set -g mouse-resize-pane on
 set -g mouse-select-pane on
 #set -g mouse-select-window on
 EOF
